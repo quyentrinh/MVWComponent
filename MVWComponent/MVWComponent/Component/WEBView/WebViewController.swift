400db404b9e5aacdb55168281b8522b80edbd5d9
DE-QuyenTrinh <quyen.trinh@vn.defide.co.jp>
2018-07-04 10:54:50 +0700

    Update webview toolbar buttons.

--- MVWComponent/MVWComponent/Component/WEBView/WebViewController.swift
+++ MVWComponent/MVWComponent/Component/WEBView/WebViewController.swift
@@ -11,12 +11,58 @@ import WebKit
 
 class WebViewController: UIViewController {
 
-    //Property
+    // UI
     private var webView: WKWebView!
     private var progressView: UIProgressView!
     
-    var URL: URL?
+    lazy var backBarButtonItem: UIBarButtonItem =  {
+        var tempBackBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_back"),
+                                                    style: UIBarButtonItemStyle.plain,
+                                                    target: self,
+                                                    action: #selector(goBackTapped))
+        return tempBackBarButtonItem
+    }()
     
+    lazy var forwardBarButtonItem: UIBarButtonItem =  {
+        var tempForwardBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "ic_menu_arrow"),
+                                                       style: UIBarButtonItemStyle.plain,
+                                                       target: self,
+                                                       action: #selector(goForwardTapped))
+        return tempForwardBarButtonItem
+    }()
+    
+    lazy var refreshBarButtonItem: UIBarButtonItem = {
+        var tempRefreshBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.refresh,
+                                                       target: self,
+                                                       action: #selector(reloadTapped))
+        return tempRefreshBarButtonItem
+    }()
+    
+    lazy var stopBarButtonItem: UIBarButtonItem = {
+        var tempStopBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.stop,
+                                                    target: self,
+                                                    action: #selector(stopTapped))
+        return tempStopBarButtonItem
+    }()
+    
+    lazy var actionBarButtonItem: UIBarButtonItem = {
+        var tempActionBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.action,
+                                                      target: self,
+                                                      action: #selector(actionButtonTapped))
+        return tempActionBarButtonItem
+    }()
+    
+    lazy var fixedSpace: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.fixedSpace, target: nil, action: nil)
+    }()
+    
+    lazy var flexibleSpace: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+    }()
+    
+    //Property
+    
+    var URL: URL?
     var loadingTime: Timer?
     var finishLoading: Bool = false
     
@@ -40,7 +86,7 @@ class WebViewController: UIViewController {
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         if let bound = navigationController?.navigationBar.bounds {
-            self.progressView.frame = CGRect(x: 0, y: bound.height - 2, width: bound.width, height: 2)
+            self.progressView.frame = CGRect(x: 0, y: bound.height - 1, width: bound.width, height: 1)
         }
     }
     
@@ -65,6 +111,7 @@ class WebViewController: UIViewController {
             scrollView.scrollIndicatorInsets.bottom = valueChange
             scrollView.contentInset.bottom = valueChange
         }
+        updateToolbarItems()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -90,7 +137,7 @@ class WebViewController: UIViewController {
         
         let progressView = UIProgressView(progressViewStyle: .default)
         progressView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin]
-        progressView.progressTintColor = .red
+        progressView.progressTintColor = #colorLiteral(red: 0.2588235294, green: 0.5215686275, blue: 0.9568627451, alpha: 1)
         progressView.trackTintColor = .clear
         if let nav = navigationController?.navigationBar {
             nav.addSubview(progressView)
@@ -112,6 +159,26 @@ class WebViewController: UIViewController {
         }
     }
     
+    func updateToolbarItems() {
+        backBarButtonItem.isEnabled = webView.canGoBack
+        forwardBarButtonItem.isEnabled = webView.canGoForward
+        
+        let refreshStopBarButtonItem: UIBarButtonItem = webView.isLoading ? stopBarButtonItem : refreshBarButtonItem
+
+        let items: NSArray = [fixedSpace, backBarButtonItem, flexibleSpace, forwardBarButtonItem, flexibleSpace, refreshStopBarButtonItem, flexibleSpace, actionBarButtonItem, fixedSpace]
+        
+        if let navigationController = navigationController {
+            if presentingViewController == nil {
+                navigationController.toolbar.barTintColor = navigationController.navigationBar.barTintColor
+            }
+            else {
+                navigationController.toolbar.barStyle = navigationController.navigationBar.barStyle
+            }
+            navigationController.toolbar.tintColor = navigationController.navigationBar.tintColor
+            toolbarItems = items as? [UIBarButtonItem]
+        }
+    }
+    
     //MARK:- OBSERVE
 
     
@@ -127,6 +194,28 @@ class WebViewController: UIViewController {
 }
 
 extension WebViewController {
+    
+    @objc func goBackTapped() {
+        webView.goBack()
+    }
+    
+    @objc func goForwardTapped() {
+        webView.goForward()
+    }
+    
+    @objc func reloadTapped() {
+        webView.reload()
+    }
+    
+    @objc func stopTapped() {
+        webView.stopLoading()
+        updateToolbarItems()
+    }
+    
+    @objc func actionButtonTapped() {
+        
+    }
+    
     @objc func dismissWebView() {
         guard let navController = navigationController else { return
             dismiss(animated: true, completion: nil)
@@ -176,20 +265,22 @@ extension WebViewController: WKNavigationDelegate {
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         finishProgress()
+        updateToolbarItems()
     }
     
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         startProgress()
+        updateToolbarItems()
     }
     
     func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         finishProgress()
-        print(error.localizedDescription)
+        updateToolbarItems()
     }
     
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
         finishProgress()
-        print(error.localizedDescription)
+        updateToolbarItems()
     }
 }
 
